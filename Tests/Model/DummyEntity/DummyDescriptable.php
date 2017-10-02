5e5a9892141335a26e6f2c298594f693b4452043
Jacek Siciarek <siciarek@gmail.com>
2017-10-02 19:10:27 +0200

    Update travis configuration

--- Tests/Model/DummyEntity/DummyDescriptable.php
+++ Tests/Model/DummyEntity/DummyDescriptable.php
@@ -3,8 +3,19 @@
 namespace Siciarek\SymfonyCommonBundle\Tests\Model\DummyEntity;
 
 use Siciarek\SymfonyCommonBundle\Model as DBBehavior;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ */
 class DummyDescriptable
 {
     use DBBehavior\Descriptable\Descriptable;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 }
--- Tests/Model/DummyEntity/DummyExtraData.php
+++ Tests/Model/DummyEntity/DummyExtraData.php
@@ -3,8 +3,19 @@
 namespace Siciarek\SymfonyCommonBundle\Tests\Model\DummyEntity;
 
 use Siciarek\SymfonyCommonBundle\Model as DBBehavior;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ */
 class DummyExtraData
 {
     use DBBehavior\ExtraData\ExtraData;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 }
